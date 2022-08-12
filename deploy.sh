eec46ab820ef2906a305d3d52d31adaa8996f253
zoujiajin <zoujiajin@banggood.com>
2022-08-12 16:42:59 +0800

    new build

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,12 @@ npm run docs:build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:GaJunZou/tps-cpn.git master:gh-pages
+git push -f https://github.com/GaJunZou/tps-cpn.git master:gh-pages
 
 cd -
